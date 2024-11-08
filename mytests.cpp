232a95551a06b874a3ffa89c2140019699fe1856
andmccutchan <andmccutchan2004@gmail.com>
2024-11-07 19:22:43 -0500

    fixed remove

--- mytests.cpp
+++ mytests.cpp
@@ -9,20 +9,27 @@ void testBST_insert(int &passCounter);
 void testBST_remove(int &passCounter);
 void testBST_search(int &passCounter);
 void testBST_min_max(int &passCounter);
+void testRBTree_insert(int &passCounter);
+void testRBTree_remove(int &passCounter);
+void testRBTree_search(int &passCounter);
+void testRBTree_min_max(int &passCounter);
 
 int main(void){
     int passCounter = 0;
-    cout << "this is working" << endl;
+
     // BST Tests
-    // testBST_insert(passCounter);
+    testBST_insert(passCounter);
     testBST_remove(passCounter);
-    // testBST_search(passCounter);
-    // testBST_min_max(passCounter);
+    testBST_search(passCounter);
+    testBST_min_max(passCounter);
 
     // RBT Tests
-    // ...
+    testRBTree_insert(passCounter);
+    testRBTree_remove(passCounter);
+    testRBTree_search(passCounter);
+    testRBTree_min_max(passCounter);
 
-    cout << "Total tests passed: " << passCounter << endl;
+    cout << "Total tests passed: " << passCounter << "/8" << endl;
 
     return 0;
 }
@@ -137,4 +144,125 @@ void testBST_min_max(int &passCounter) {
     passCounter++;
 }
 
+void testRBTree_insert(int &passCounter){
+    cout << "Test Code for Insert" << endl;
+    cout << "===========================" << endl;
+    cout << "Inserting 15, 22, 18, 10, 5, 8, 30..." << endl;
+    RBTree<int> RBTree;
+    RBTree.insert(15);
+    RBTree.insert(22);
+    RBTree.insert(18);
+    RBTree.insert(10);
+    RBTree.insert(5);
+    RBTree.insert(8);
+    RBTree.insert(30);
+
+    cout << "Tree size: " << RBTree.size() << endl;
+
+    cout << "Pre-Order traversal: ";
+    RBTree.printPreOrderTraversal();
+    cout << endl;
+    cout << "In-Order traversal: ";
+    RBTree.printInOrderTraversal();
+    cout << endl;
+    cout << "Post-Order traversal: ";
+    RBTree.printPostOrderTraversal();
+    
+    cout << endl;
+    cout << "===========================" << endl;
+    cout << endl;
+    passCounter++;
+}
+
+void testRBTree_remove(int &passCounter){
+    cout << "Test Code for Remove" << endl;
+    cout << "===========================" << endl;
+    cout << "Inserting 15, 22, 18, 10, 5, 8, 30..." << endl;
+    RBTree<int> RBTree;
+    RBTree.insert(15);
+    RBTree.insert(22);
+    RBTree.insert(18);
+    RBTree.insert(10);
+    RBTree.insert(5);
+    RBTree.insert(8);
+    RBTree.insert(30);
+    RBTree.insert(17);
+    RBTree.insert(19);
+    RBTree.insert(2);
+
+    cout << "Tree size: " << RBTree.size() << endl;
+    cout << "Pre-Order traversal: ";
+
+    RBTree.printPreOrderTraversal();
+    cout << endl;
+    cout << "In-Order traversal: ";
+    RBTree.printInOrderTraversal();
+    cout << endl;
+    cout << "Post-Order traversal: ";
+    RBTree.printPostOrderTraversal();
+    cout << endl;
+
+    cout << "Remove 10..." << endl;
+    RBTree.remove(5);
+
+    cout << "New Tree Size: " << RBTree.size() << endl;
+
+    cout << "In-Order traversal: ";
+    RBTree.printInOrderTraversal();
+    cout << endl;
+    cout << "Post-Order traversal: ";
+    RBTree.printPostOrderTraversal();
+
+    cout << endl;
+    cout << "===========================" << endl;
+    cout << endl;
+    passCounter++;
+}
+
+void testRBTree_search(int &passCounter){
+    cout << "Test Code for Search" << endl;
+    cout << "===========================" << endl;
+    cout << "Inserting 15, 22, 18, 10, 5, 8, 30..." << endl;
+    RBTree<int> RBTree;
+    RBTree.insert(15);
+    RBTree.insert(22);
+    RBTree.insert(18);
+    RBTree.insert(10);
+    RBTree.insert(5);
+    RBTree.insert(8);
+    RBTree.insert(30);
+
+    cout << "Search for the value 18..." << endl;
+    (RBTree.search(18) != nullptr) ? cout << "Found!" << endl : cout << "Not Found" << endl;
+    cout << "Search for the value 5..." << endl;
+    (RBTree.search(5) != nullptr) ? cout << "Found!" << endl : cout << "Not Found" << endl;
+
+    cout << "===========================" << endl;
+    cout << endl;
+    passCounter++;
+}
+
+void testRBTree_min_max(int &passCounter){
+    cout << "Test Code for Minimum and Maximum" << endl;
+    cout << "===========================" << endl;
+    cout << "Inserting 15, 22, 18, 10, 5, 8, 30..." << endl;
+    RBTree<int> RBTree;
+    RBTree.insert(15);
+    RBTree.insert(22);
+    RBTree.insert(18);
+    RBTree.insert(10);
+    RBTree.insert(5);
+    RBTree.insert(8);
+    RBTree.insert(30);
+
+    RBTreeNode<int>* RBTmin = RBTree.treeMin();
+    cout << "Tree Min: " << RBTmin -> data << endl;
+
+    RBTreeNode<int>* RBTmax = RBTree.treeMax();
+    cout << "Tree Max: " << RBTmax -> data << endl;
+
+    cout << "===========================" << endl;
+    cout << endl;
+    passCounter++;
+}
 
